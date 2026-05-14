9164fb5c40ad158bc06da84f841993bfa1e82862
BlockShip <1521505611@qq.com>
2026-05-14 10:34:22 +0800

    feat: integrate StarField and ShootingStar

--- src/components/background/StarryNightBackground.tsx
+++ src/components/background/StarryNightBackground.tsx
@@ -1,8 +1,13 @@
+import { StarField } from './StarField';
+import { ShootingStar } from './ShootingStar';
+
 export function StarryNightBackground() {
   return (
     <div className="absolute inset-0 night-sky-gradient overflow-hidden">
       <div className="moon-glow" />
       <div className="moon-core" />
+      <StarField />
+      <ShootingStar />
     </div>
   );
 }
